17df8ec2883fe820654ea4e1e989593bd4b10bf4
Harish Hothi <harish_hothi@affirmed.local>
2018-04-20 00:34:59 +0530

    Added comment and int declaration

--- Hello.c
+++ Hello.c
@@ -1,6 +1,11 @@
+/**
+Should have comment, purpose of this file should be describe
+*/
 #include<stdio.h>
 #include<conio.h>
 void main(){
+	int no = 1;
+	int mul = 5;
 	Prinf("Hello world");
   	Prinf("Welcome in a world of C");
 }
